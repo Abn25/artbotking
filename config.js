d2d1dadf0600798a418a53714312d7c3b614c48c
Abn25 <60712879+Abn25@users.noreply.github.com>
2020-03-11 17:52:47 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NjQ0OTczNTczMzMyNTMzMjc2.XmUz6A.DAGoY8EqFXJyDGEOxLvIN2Kx-1Q",
+  YT_API_KEY: "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4",
+  prefix: "$",
+  devs: ["532325977921945603"]
+};
